7095d46c145838d28d9bf9869c4124db73a9a1f2
hannah-thorley <hannahthorley@Unknown-60-c5-47-93-fa-44.home>
2017-03-05 10:54:05 +0000

    Initial commit

---
+++ js/jquery.js
@@ -0,0 +1,42 @@
+
+//delay code from executing until all assets loaded
+
+//if read more is clicked
+//prevent default action
+	//slide p down
+	//hide read more
+	//show show less
+
+$(document).ready(function() {
+
+$('.readmore').on('click', function(event) {
+	event.preventDefault();
+	$('#show-this-on-click').slideDown();
+	$('.readmore').hide();
+	$('.readless').show();
+});
+
+//if read less is clicked
+//prevent default action
+	//slide p up
+	//hide read less
+	//show read more
+
+$('.readless').on('click', function(event) {
+	event.preventDefault();
+	$('#show-this-on-click').slideUp();
+	$('.readless').hide();
+	$('.readmore').show();
+});
+
+//if learn more is clicked
+//prevent default action
+	//slide span down
+	//hide learn more
+
+$('.learnmore').on('click', function(event) {
+	event.preventDefault();
+	$('#learnmoretext').slideDown();
+	$('.learnmore').hide();
+});
+});
